4316ae9568df2089d5e71271c914859a1bb853fd
Som Ramnani <som.ramnani@gmail.com>
2019-05-19 12:51:52 -0500

    added working code and images

---
+++ bamazonCustomer.js
@@ -0,0 +1,118 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+var Table =  require("cli-table2");
+var colors = require("colors")
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "Dragonfly_23",
+  database: "bamazon"
+})
+
+connection.connect(function(err) {
+  if(err) throw err;
+  console.log("connected as id " + connection.threadId);
+  
+
+});
+
+function getTable() {
+  console.log('\n')
+  var query = "SELECT * FROM products";
+  connection.query(query, function(err, res) {
+    let table = new Table({
+      head:
+        [
+          'ID'.bold, 
+          'Item Description'.bold, 
+          {
+            hAlign: 'center', 
+            content: 'Department'.bold
+          }, 
+            
+            {
+              hAlign: 'center', 
+              content: 'Price'.bold
+            }, 
+            'In Stock'.bold
+          ],
+
+      colWidths: [5,100,25,10,10]
+    })
+    for (var i = 0; i < res.length; i++) {
+     
+        table.push(
+          [colors.cyan.bold(res[i].id), 
+          res[i].product_name, 
+          res[i].depart_name,
+          {
+            hAlign:'right', 
+            content :'$'+parseFloat(res[i].price).toFixed(2)
+          }, 
+          {
+            hAlign: 'right', 
+            content: parseInt(res[i].stock_quantity)
+          }
+        ])
+    
+    }
+    console.log(table.toString())
+    purchasePrompt();
+    });
+  }
+
+
+  function purchasePrompt(){
+    inquirer
+    .prompt([
+      {
+        name: "itemID",
+        type: "input",
+        message: "What is the ID of the product you would like to buy?"
+      },
+      {
+        name: "quantity",
+        type: "input",
+        message: "How many would you like to buy?"
+      }
+    ])
+    .then(function(answer){
+      var userID = answer.itemID;
+      var quantity = answer.quantity;
+      purchaseOrder(userID, quantity);   
+    });
+
+  }
+
+  function purchaseOrder(ID, amountNeeded){
+    connection.query("SELECT * FROM products WHERE id = " + connection.escape(ID), function(err,res){
+      if(err) throw err;
+    
+      if(amountNeeded <= res[0].stock_quantity){
+        var total = res[0].price * amountNeeded;
+        var newQty = res[0].stock_quantity - amountNeeded;
+        console.log("Your total cost for " + amountNeeded +" " + res[0].product_name + "is $" + total);
+        connection.query("UPDATE products SET stock_quantity = " + newQty + "WHERE id = " + connection.escape(ID), function(err,res){
+          if(err) throw err;
+        } );
+      }
+    
+      else{
+        console.log("Insufficient quantity! We do not have enough of " + res[0].product_name );
+      }
+
+      getTable();
+ 
+  });
+}
+getTable();
+ 
+     
+   
+
+
+
+
+
--- bamazonSchema.sql
+++ bamazonSchema.sql
@@ -9,3 +9,17 @@ CREATE TABLE products(
     stock_quanity INTEGER,
     PRIMARY KEY(id)
 )
+
+SELECT * FROM products;
+INSERT INTO products (product_name, depart_name,  price, stock_quantity)
+VALUES  ( "Marvel’s Spider-Man - PlayStation 4  ","Video Games", 9.99, 50),
+        ( "OPTIMUM NUTRITION GOLD STANDARD 100% Whey Protein Powder, Double Rich Chocolate, 5 Pound  ","Health & Household ", 55.00, 50),
+        (" BeatsX Earphones - Black","Electronics",  100, 100),
+        ("APC 11-Outlet Surge Protector Power Strip with USB Charging Ports","Electronics",  30.00, 10),
+        ( " Google Chromecast (3rd Generation)","Electronics", 35.00, 50),
+        ( " Adidas Squeeze 750ML Plastic Water Bottle (28oz) ","Sports & Outdoors", 10.00, 50),
+        ("TR Industrial TR88302 Multi-Purpose Cable Tie (100 Piece), 8  ","Accessories & Supplies",  7.00, 50),
+        ( "  American Crew Forming Cream, 3 Ounce (Pack of 4)","Beauty & Personal Care", 38.00, 50),
+        ( " Batman: Hush  ","Books", 17.00, 50),
+        ( "Tent", "Outdoors", 39.99,  10);
+
